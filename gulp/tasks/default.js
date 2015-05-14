be3334a2ccc859ae3e06a4f670ce07ac8271117b
Robert McGuinness <rob.mcguinness@availity.com>
2015-05-14 03:15:46 -0400

    run certain gulp tasks sequentially

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence').use(gulp);
 
-gulp.task('default', [
-  'less',
-  'copy',
-  'concat',
-  'build',
-  'server',
-  'watch'
-]);
+gulp.task('default', function() {
+  runSequence(
+    ['less', 'copy', 'concat'],
+    'build',
+    'server',
+    'watch'
+  );
+});
