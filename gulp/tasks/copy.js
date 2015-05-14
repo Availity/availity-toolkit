8f0d8ff8d47ab080c3148554a546718740074c27
Robert McGuinness <rob.mcguinness@availity.com>
2015-05-14 02:12:49 -0400

    remove auto reloading of templates

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
 
 var config = require('../config');
 
@@ -8,6 +6,5 @@ gulp.task('copy', ['copy:templates']);
 
 gulp.task('copy:templates', function() {
   gulp.src(config.templates.src)
-    .pipe(gulp.dest(config.markup.dest))
-    .pipe(reload({ stream:true }));
+    .pipe(gulp.dest(config.markup.dest));
 });
