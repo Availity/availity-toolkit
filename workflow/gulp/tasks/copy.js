c0cc248f5c3412147b5bf4a2e6764e90a020b024
Robert McGuinness <rob.mcguinness@availity.com>
2015-06-25 00:32:47 -0400

    add cache to template copy task
    * only copy templates that have changed
    * emit file size for each template copied

--- workflow/gulp/tasks/copy.js
+++ workflow/gulp/tasks/copy.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var changed = require('gulp-changed');
+var size = require('gulp-size');
 
 var config = require('../../config');
 
@@ -6,5 +8,10 @@ gulp.task('copy', ['copy:templates']);
 
 gulp.task('copy:templates', function() {
   gulp.src(config.templates.src)
-    .pipe(gulp.dest(config.markup.dest));
+    .pipe(changed(config.markup.dest))
+    .pipe(gulp.dest(config.markup.dest))
+    .pipe(size({
+      showFiles: true,
+      title: 'copy:templates'
+    }));
 });
