0c602ef45b278d426074066261d09c3e0d4c0f58
Kasey Powers <kaseyepowers@gmail.com>
2015-07-17 10:20:49 -0400

    adding server settings to rootVars

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ var rootVars = {};
 rootVars.gulp = require('gulp');
 rootVars.karma = require('karma');
 
+rootVars.server = {
+  host: 'localhost',
+  port: '3000'
+};
+
 var workflow = require('availity-workflow');
 workflow.gulp(rootVars);
 
