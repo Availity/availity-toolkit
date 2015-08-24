392b2afe804f1a796329c6dbd45104a8b92b70d8
Rob McGuinness <rob.mcguinness@availity.com>
2015-08-23 20:35:26 -0400

    remove karma

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 
 var gulp = require('gulp');
-var karma = require('karma');
 
 var workflow = require('availity-workflow');
 
 workflow.use({
-  gulp: gulp,
-  karma: karma
+  gulp: gulp
 });
 
 gulp.task('default', ['av:default']);
