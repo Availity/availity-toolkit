978d580a2da21465614512e58159f134fbf1ad04
Rob McGuinness <rob.mcguinness@availity.com>
2015-09-15 16:37:12 -0400

    update developer config example

--- project/config/devoloper-config-example.js
+++ project/config/devoloper-config-example.js
@@ -3,84 +3,95 @@ var path = require('path');
 var developerConfig = {
 
   development: {
-    data: path.join(__dirname , '../data'),
+
+    open: '/#',
+
+    data: path.join(__dirname, '../data'),
     routes: path.join(__dirname, './routes.json'),
+
     servers: {
       web: {
         host: '0.0.0.0',
         port: 9999
       },
-      api : {
-        proxy : true,
-        port : 8280,
+      app: {
+        host: '0.0.0.0',
+        port: 3000
+      },
+      api: {
+        proxy: false,
+        port: 8280,
         headers: {
           RemoteUser: 'jdoe'
         },
-        proxies : [
+        proxies: [
           {
-            context : '/api',
-            rewrite : {
-              from : '^/api',
-              to : ''
+            context: '/api',
+            rewrite: {
+              from: '^/api',
+              to: ''
             }
           },
           {
-            context : '/public/api',
-            rewrite : {
-              from : '^/public/api',
-              to : ''
+            context: '/public/api',
+            rewrite: {
+              from: '^/public/api',
+              to: ''
             }
           }
         ]
       },
-      legacyApi : {
-        proxy : true,
-        port : 8281,
+      legacyApi: {
+        proxy: true,
+        port: 8281,
         headers: {
           RemoteUser: 'jdoe'
         },
-        proxies : [
+        proxies: [
           {
-            context : '/api\/v1\/uploads',
-            rewrite : {
-              from : '^/api/v1/uploads',
-              to : '/aries/api/v1/uploads'
+            context: '/api\/v1\/uploads',
+            rewrite: {
+              from: '^/api/v1/uploads',
+              to: '/aries/api/v1/uploads'
             }
           },
           {
-            context : '/api\/v1\/attachments',
-            rewrite : {
-              from : '^/api/v1/attachments',
-              to : '/aries/api/v1/attachments'
+            context: '/api\/v1\/attachments',
+            rewrite: {
+              from: '^/api/v1/attachments',
+              to: '/aries/api/v1/attachments'
             }
           },
           {
-            context : '/api\/v1\/receiver-attachments',
-            rewrite : {
-              from : '^/api/v1/receiver-attachments',
-              to : '/aries/api/v1/receiver-attachments'
+            context: '/api\/v1\/receiver-attachments',
+            rewrite: {
+              from: '^/api/v1/receiver-attachments',
+              to: '/aries/api/v1/receiver-attachments'
             }
           }
         ]
       },
-      "legacyWeb" : {
-        proxy : true,
-        port : 8080,
-        context : '/availity'
+      legacyWeb: {
+        proxy: true,
+        port: 8080,
+        context: '/availity'
       }
     }
   },
 
   production: {
-    data: path.join(__dirname , '../data'),
+
+    data: path.join(__dirname, '../data'),
     routes: path.join(__dirname, './routes.json'),
+
     latency: 300,
     user: null,
     cache: 86400000,
     limit: '50mb',
+
     servers: {
       web: {
-        host: "0.0.0.0",
+        host: '0.0.0.0',
         port: 9999
       }
     }
